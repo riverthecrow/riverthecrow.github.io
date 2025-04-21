6d34f4efb0c7c25b47e6903a9ec6ce6d4f9130b3
river <rivernichols129@proton.me>
2025-04-20 21:07:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-
-    overlay.addEventListener('click', () => {
+ overlay.addEventListener('click', () => {
         overlay.style.opacity = '0';
         setTimeout(() => {
             overlay.style.display = 'none';
+            content.style.display = 'flex';
+            playerContainer.style.display = 'flex';
+            loadSong(currentSongIndex);
+            updateVolumeEmoji(currentVolume);
+        }, 1000);
     });
